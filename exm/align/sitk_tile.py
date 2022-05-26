4838f54a0ba2cc2bdbac6f37cf2b5234139211f6
jackransomlovell <jlovell@mit.edu>
2022-05-25 22:11:12 -0400

    computing transform runs, not sure if it is correct

--- exm/align/sitk_tile.py
+++ exm/align/sitk_tile.py
@@ -14,13 +14,16 @@ class sitkTile:
     
     def setResolution(self, resolution):
         # xyz-order
-        self.resolution = resolution
+        VectorType = itk.Vector[itk.F, len(resolution)]
+        res_vec = VectorType()
+        for ind, res in enumerate(resolution):
+            res_vec[ind] = res
+    
+        self.resolution = res_vec
 
     #### Setup
-    def setTransformType(self, transform_type, num_iteration = -1, OpenCL = False):
-        self.transform_type = transform_type
-        self.parameter_map = self.createParameterMap(transform_type, num_iteration, OpenCL)
-        self.elastix.SetParameterMap(self.parameter_map)
+    #def setTransformType(self, transform_type, num_iteration = -1, OpenCL = False):
+    #    self.transform_type = transform_type
     
     def updateParameterMap(self, parameter_map=None):
         if parameter_map is not None:
@@ -29,6 +32,10 @@ class sitkTile:
 
     def getParameterMap(self):
         return self.parameter_map
+    
+    def setParamterMap(self):
+        self.parameter_map = self.createParameterMap(transform_type, num_iteration, OpenCL)
+        self.elastix.SetParameterObject(self.parameter_map)
 
     def readTransformMap(self, filename):
         return itk.ReadParameterFile(filename)
@@ -40,7 +47,8 @@ class sitkTile:
         if transform_type is None:
             transform_type = self.transform_type
         if len(transform_type) == 1:
-            parameter_map = itk.GetDefaultParameterMap(transform_type[0])
+            parameter_object = itk.ParameterObject.New()
+            parameter_map = parameter_object.GetDefaultParameterMap(transform_type[0])
             parameter_map['NumberOfSamplesForExactGradient'] = ['100000']
             if num_iteration > 0:
                 parameter_map['MaximumNumberOfIterations'] = [str(num_iteration)]
@@ -66,13 +74,15 @@ class sitkTile:
 
             parameter_map['OpenCLMovingGenericImagePyramidUseOpenCL'] = ["true"]
             parameter_map['MovingImagePyramid'] = ["OpenCLMovingGenericImagePyramid"]
+            
+            self.parameter_map = parameter_map
 
-        return parameter_map
+        return self.parameter_map
 
     #### Estimate and warp with transformation
-    def convertSitkImage(self, vol_np, res_np):
+    def convertSitkImage(self, vol_np, res):
         vol = itk.GetImageFromArray(vol_np)
-        vol.SetSpacing(res_np)
+        vol.SetSpacing(res)
         return vol
     
     def computeMask(vol, percentile = 60):
@@ -85,24 +95,28 @@ class sitkTile:
         # https://github.com/SuperElastix/SimpleElastix/issues/198
         # not enough samples in the mask
         #self.elastix.SetLogToConsole(False)
-        self.elastix.SetLogToConsole(True)
+        
+        # 1. load imgs 
         if res_fix is None:
             res_fix = self.resolution
         if res_move is None:
             res_move = self.resolution
-        # 2. load volume
-        # print('vol-fix shape:', vol_fix.shape)
-        vol_fix = self.convertSitkImage(vol_fix, res_fix)
-        self.elastix.SetFixedImage(vol_fix)
+            
+        img_fix = self.convertSitkImage(vol_fix, res_fix)
+        img_move = self.convertSitkImage(vol_move, res_move)
+        
+        # 2. set elastix obj
+        self.elastix = itk.ElastixRegistrationMethod.New(img_fix,img_move)
+        self.elastix.SetParameterObject(self.parameter_map)
+        self.elastix.SetLogToConsole(True)
+
+        # 3. set masks
         if mask_fix is not None:
             self.elastix.SetParameter("ImageSampler", "RandomSparseMask")
             mask_fix = self.convertSitkImage(mask_fix, res_fix)
             mask_fix.CopyInformation(vol_fix)
             self.elastix.SetFixedMask(mask_fix)
 
-        # print('vol-move shape:', vol_move.shape)
-        vol_move = self.convertSitkImage(vol_move, res_move)
-        self.elastix.SetMovingImage(vol_move)
         if mask_move is not None:
             #self.elastix.SetParameter("ImageSampler", "RandomSparseMask")
             mask_move = self.convertSitkImage(mask_move, res_move)
@@ -110,8 +124,8 @@ class sitkTile:
             self.elastix.SetMovingMask(mask_move)
             
         # 3. compute transformation
-        self.elastix.Execute()
-
+        self.elastix.UpdateLargestPossibleRegion()
+        
         # 4. output transformation parameter
         return self.elastix.GetTransformParameterMap()[0]
         
@@ -119,8 +133,10 @@ class sitkTile:
         self.transformix.SetLogToConsole(True)
         if res_move is None:
             res_move = self.resolution
+            
         self.transformix.SetTransformParameterMap(transform_map)
         self.transformix.SetMovingImage(self.convertSitkImage(vol_move, res_move))
-        self.transformix.Execute()
+        self.transformix.UpdateLargestPossibleRegion()
+        
         out = sitk.GetArrayFromImage(self.transformix.GetResultImage())
         return out
