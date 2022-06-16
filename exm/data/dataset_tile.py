9053779b6b9328fc620fd0d73444a381e682b22a
jackransomlovell <jlovell@mit.edu>
2022-06-16 19:37:09 -0400

    dataset object working, returns fovXzXyXz obj, 25 ms increase compared to nd2ToVol

--- exm/data/dataset_tile.py
+++ exm/data/dataset_tile.py
@@ -1,64 +1,27 @@
-from pims import ND2_Reader
+from nd2reader import ND2Reader
 import h5py
 import os
 
 class datasetTile:
 
     def __init__(self):
-        self.vol_fix = None
-        self.vol_move = None
-        self.vol_h5 = None
+        self.vol = None
     
-    def loadVols(self, vol_fix_path: str, vol_mov_path: str, channel: str = None, result_path: str = None, iter_axes: str = None):
+    def loadVols(self, vol_path: str, iter_axes: str = 'v', channel: str = None):
 
-        if '.nd2' in vol_fix_path:
+        if '.nd2' in vol_path:
 
-            vol_fix = ND2_Reader(vol_fix_path)
+            vol = ND2Reader(vol_path)
 
             if iter_axes:
-                vol_fix.iter_axes = iter_axes
-
-            else:
-                axes = ['x','y','z']
-                iter_axes = set(vol_fix.sizes.keys()) - set(axes)
-
-                assert len(iter_axes) == 1, "can only set one iter axis, should be channel or fov"
-
-                vol_fix.iter_axes = iter_axes
+                vol.iter_axes = iter_axes
     
-            self.vol_fix = vol_fix
+            self.vol = vol
 
-        if '.nd2' in vol_mov_path:
-
-            vol_mov = ND2_Reader(vol_mov_path)
-
-            if iter_axes:
-                vol_mov.iter_axes = iter_axes
-
-            else:
-                axes = ['x','y','z']
-                iter_axes = set(vol_mov.sizes.keys()) - set(axes)
-
-                assert len(iter_axes) == 1, "can only set one iter axis, should be channel or fov"
-
-                vol_mov.iter_axes = iter_axes
+        elif '.h5' in vol_path:
+            self.vol = h5py.File(vol_path, 'r')[channel]
     
-            self.vol_mov = vol_mov
-
-
-        elif '.h5' in vol_fix_path:
-            self.vol_fix = h5py.File(vol_fix_path, 'r')[channel]
-
-        elif '.h5' in vol_mov_path:
-            self.vol_move = h5py.File(vol_mov_path, 'r')[channel]
-
-        if result_path:
-            self.vol_h5 = h5py.File(result_path, 'r')[channel]
-    
-        return self.vol_fix, self.vol_move, self.vol_h5
-
-    def setResult(self, result):
-        self.vol_h5 = result
+        return self.vol
 
     def setMasks(self, mask_fix, mask_move = None):
         
