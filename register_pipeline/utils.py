82e7c166e58775a59e993a14c4d18a9810a3a948
atomic-rooster <chaitanya.7.kapoor@gmail.com>
2022-07-06 21:49:57 -0400

    add ROI, fix import bugs in utils

--- register_pipeline/utils.py
+++ register_pipeline/utils.py
@@ -1,4 +1,7 @@
-from tiffile import imread, imsave
+import os
+import h5py
+import numpy as np
+from tifffile import imwrite
 
 def createTIFF(f_vol_out: str, channel_names: list):
     '''
@@ -6,6 +9,7 @@ def createTIFF(f_vol_out: str, channel_names: list):
           channel_names -> channel names in the volume
     returns a numpy image array
     '''
+    # make more modular (change needed)
     output = np.zeros((4, 81, 2048, 2048)).astype(np.uint16)
 
     for i in range(len(channel_names)):
@@ -25,6 +29,28 @@ def writeTIFF(img_name: str, work_path: str, np_img: np.ndarray, channel_names: 
     writes a TIFF file
     '''
     os.chdir(work_path)
-    tiffile.imwrite(img_name, np_img,
+    imwrite(img_name, np_img,
                     metadata={'Channel': {'Name': channel_names}})
+
+
+def createROI(im_fpath: str, ROI: list):
+    '''
+    create a ROI within specified coordinate range
+    '''
+    assert len(ROI) == 4, "(x1, x2, y1, y2) coordinate limits for ROI"
+
+    im = tiffile.imread(im_fpath)
+    im_ROI = list()
+    for i in range(im.shape[0]):
+        channel = im[:,i]
+        slice_ROI = list()
+        for j in range(channel.shape[0]):
+            im_slice = channel[i, :, :,]
+            slice_ROI.append(im_slice[ROI[0]:ROI[1], ROI[2]:ROI[3]:,])
+        im_ROI.append(np.array(slice_ROI))
+
+    im_ROI = np.array(im_ROI)
+    im_ROI = np.swapaxes(im_ROI, 0, 1)
+
+    return np.array(im_ROI)
 
