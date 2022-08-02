22be9fb78ba00f019e05685543cb3d5d7daa5009
atomic-rooster <chaitanya.7.kapoor@gmail.com>
2022-08-02 10:32:50 +0530

    base configs for filtering, point and intensity reg

--- joint/config/default.py
+++ joint/config/default.py
@@ -12,17 +12,28 @@ _C.SYSTEM = CN()
 # filter
 _C.FILTER = CN()
 # thresholding, denoising and masking params
-
-
+_C.FILTER.FILTER_STRENGTH = 70
+_C.FILTER.THRESH_LOWER = 60
+_C.FILTER.THRESH_UPPER = 255
+_C.FILTER.MASK_INDEX = 6
+_C.FILTER.DELTA = 230
 
 # intensity
 _C.INTENSITY = CN()
 # elastix params
-
+_C.INTENSITY.RESOLUTION = [0.1625, 0.1625, 0.2500]
+_C.INTENSITY.TRANSFORM_TYPE = ['affine']
+_C.INTENSITY.NumberOfSamplesForExactGradient = '100000'
+_C.INTENSITY.MaximumNumberOfIterations = '10000'
+_C.INTENSITY.MaximumNumberOfSamplingAttempts = '15'
+_C.INTENSITY.FinalBSplineInterpolationOrder = '1'
 
 # point
 _C.POINT = CN()
-# point-based regsitration params
+# point-based registration params
+_C.POINT.NUM_POINTS = 7
+_C.POINT.MAX_ITER = 50
+_C.POINT.TOLERANCE = 1
 
 # dataset
 _C.DATASET = CN()
