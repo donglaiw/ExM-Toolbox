66633664fb9bf0edbaa4ea83a0dd96385fe9f8e1
atomic-rooster <chaitanya.7.kapoor@gmail.com>
2022-11-15 21:58:09 +0530

    everything put together

--- joint/main.py
+++ joint/main.py
@@ -2,35 +2,35 @@ import json
 import time
 from intensity.run_coarse import *
 from config.utils import load_cfg
-from point.run_fine import warp_image
+from point.run_fine import warpImage
+from intensity.run_coarse import runCoarse
 
 
-# run coarse and fine registration on a single image volume
-# which is specified in the configuration file
+def main():
+    # run coarse and fine registration on a single image volume
+    # which is specified in the configuration file
 
-# load configuration
-cfg = load_cfg()
-print(f"Current configration used:")
-print(json.dumps(cfg, indent=4))
+    # load configuration
+    cfg = load_cfg()
+    print(f"Current configration used:")
+    print(json.dumps(cfg, indent=4))
+
+    # run coarse alignment
+    print(f"\n\nrunning coarse alignment...")
+    tick = time.time()
+    runCoarse(cfg)
+    tock = time.time()
+    print(f"Coarse alignment over!")
+    print(f"{(tock - tick):.4f} seconds for coarse alignment!\n")
+
+    # run fine alignment
+    print(f"running fine alignment...")
+    tick = time.time()
+    warpImage(cfg)
+    tock = time.time()
+    print(f"Fine alignment over!")
+    print(f"{(tock - tick):.4f} seconds for fine alignment!")
 
 
-# create results directory structure
-# createFolderStruc(out_dir='results/')
-
-# run coarse alignment
-print(f"\n\nrunning coarse alignment...")
-tick = time.time()
-# <run coarse registration>
-tock = time.time()
-print(f"Coarse alignment over!")
-print(f"{tock - tick} seconds for coarse alignment!\n")
-
-# run fine alignment
-print(f"running fine alignment...")
-
-tick = time.time()
-warp_image(cfg)
-tock = time.time()
-
-print(f"Fine alignment over!")
-print(f"{tock - tick} seconds for fine alignment!")
+if __name__ == "__main__":
+    main()
